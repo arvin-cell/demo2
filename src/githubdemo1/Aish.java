14986e2f3c793641aa8242ebd0888ae4bffbce8f
Lenovo <Lenovo@LAPTOP-EL3V1AMU>
2020-12-15 16:56:19 +0530

    commit 3rd

--- src/githubdemo1/Aish.java
+++ src/githubdemo1/Aish.java
@@ -5,6 +5,7 @@ public class Aish
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		System.out.println("my name is arvind not aishu ples change");
+		System.out.println("sorry dear my mistake");
 		
 	}
 
